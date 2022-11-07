e6ab9882ef77509f42041e4a35dbe7927806745a
Renato Vianna <marquez.vianna@gmail.com>
2022-11-07 20:23:10 -0300

    Desafio Dio.me

---
+++ src/ChamadaRecursiva.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ChamadaRecursiva {
+
+    public static void main(String[] args) {
+
+        Scanner numero = new Scanner(System.in);
+
+        int N = numero.nextInt();
+
+        int soma = somatorio(N);
+
+        System.out.println(soma);
+    }
+            static int somatorio(int numero) {
+                if (numero == 0) {
+                    return 0;
+                } else {
+                    return numero + somatorio(numero - 1);
+                }
+            }
+        
+}
