ad93dce9ba5216e459601c21762eb8adaaef5037
Michelle <michelle.g.vianna@gmail.com>
2023-03-10 15:31:30 -0300

    Novos Desafios

---
+++ src/Fatorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.print(fatorial + "! = ");
+        for (int i = fatorial; i >= 1; i--){
+            multiplicacao = multiplicacao * i;
+        }
+
+        System.out.println(multiplicacao);
+    }
+}
