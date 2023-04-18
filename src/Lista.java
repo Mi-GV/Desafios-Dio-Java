a17023cd0ae83aa64d4d49d26a0590d48a609297
Michelle <michelle.g.vianna@gmail.com>
2023-04-18 09:33:18 -0300

    Novo Desafio

--- src/Lista.java
+++ src/Lista.java
@@ -1,3 +1,64 @@
+/*
+Desafio
+Faça um programa com as características abaixo:
+
+Leia 10 nomes, sem espaço em branco;
+Imprima o terceiro nome da lista;
+Imprima o sétimo nome da lista;
+Imprima o nono nome da lista.
+
+Entrada:
+A entrada consiste vários arquivos de teste, cada um com dez linhas e em cada linha tem um nome de no até 30 caracteres e sem espaço em branco.
+Conforme mostrado no exemplo de entrada a seguir.
+
+Saída:
+Para cada arquivo da entrada, terá um arquivo de saída. E como mencionado no Desafio, gere três linhas conforme os procedimentos 2, 3 e 4.
+Use o exemplo abaixo para clarear o que está sendo pedido.
+
+
+Exemplos de Entrada	  Exemplos de Saída
+USP                   UFCG
+UFPE                  ITA
+UFCG                  URI
+UFRN
+UFRJ
+IME
+ITA
+UNIOESTE
+URI
+UFG
+
+UnB                              UNIFEI
+UFMG                             UFRGS
+UNIFEI                           UFU
+UECE
+UNICAMP
+INATEL
+UFRGS
+UNIFESO
+UFU
+PUC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
 import java.util.Scanner;
 
 public class Lista {
